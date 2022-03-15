34e9ed8ecb247443b226e3960c59c7f4473bc7fb
nttuu99@gmail.com <uphu99qn11>
2022-03-15 13:58:21 +0700

    fix CkeckButton

--- src/conponents/CheckButton.js
+++ src/conponents/CheckButton.js
@@ -16,7 +16,7 @@ const boxVariants = {
   },
   unchecked: { background: 'var(--gray-1)', transition: { duration: 0.1 } },
 };
-function CheckButton({ checked, setChecked, handleCheck }) {
+function CheckButton({ checked, handleCheck }) {
   const pathLength = useMotionValue(0);
   const opacity = useTransform(pathLength, [0.05, 0.15], [0, 1]);
   return (
